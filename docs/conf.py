e1c41af7ef820efba5049715f58daa946b5d310b
Joseph Wayne Pardue <joepardue47@gmail.com>
2025-06-24 14:26:17 -0400

    Add comprehensive Picowide documentation

--- docs/conf.py
+++ docs/conf.py
@@ -1,16 +1,13 @@
-# conf.py - CircuitPython Application Documentation
-
-# Project information
-project = 'picowide'
+# Picowide Documentation Configuration
+project = 'Picowide'
 copyright = '2025, Joseph Wayne Pardue'
 author = 'Joseph Wayne Pardue'
-release = '0.1.0'
+release = '1.0'
 
-# -- General configuration ---------------------------------------------------
 extensions = []
-
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-# -- Options for HTML output -------------------------------------------------
 html_theme = 'sphinx_rtd_theme'
+html_title = 'Picowide: Your Pico W\'s Portable Web IDE'
+html_short_title = 'Picowide'
